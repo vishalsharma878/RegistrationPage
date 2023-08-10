fdc5c192e87230cf3c3c4660c5021db8f82aa0fb
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-10 16:35:14 +0530

    New Changes

--- main.js
+++ main.js
@@ -14,5 +14,14 @@ const addItem = document.querySelector('.title');
 addItem.style.fontFamily = 'bold';
 addItem.style.color ='green';
 
+const items = document.getElementsByClassName('list-group-item');
+
+items[1].style.backgroundColor = "green";
+
+for(let i =0; i<items.length; i++){
+    items[i].style.fontWeight = "bold";
+}
+
+
 
 
