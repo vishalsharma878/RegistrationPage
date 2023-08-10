528bbd59805162da0034da84d46d277630f18bce
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-10 17:42:23 +0530

    Changes

--- main.js
+++ main.js
@@ -9,18 +9,32 @@ console.log(headerTitle);
 //headerTitle.innerHTML ='<h3>Hello</>'; 
 //headerTitle.style.borderBottom = 'solid 3px black';
 
-const addItem = document.querySelector('.title');
+// const addItem = document.querySelector('.title');
 
-addItem.style.fontFamily = 'bold';
-addItem.style.color ='green';
+// addItem.style.fontFamily = 'bold';
+// addItem.style.color ='green';
 
-const li = document.getElementsByTagName('li');
+// const li = document.getElementsByTagName('li');
 
-li[1].style.backgroundColor = "green";
+// li[1].style.backgroundColor = "green";
 
-for(let i =0; i<li.length; i++){
-    li[i].style.fontWeight = "bold";
+// for(let i =0; i<li.length; i++){
+//     li[i].style.fontWeight = "bold";
+// }
+
+const item = document.querySelector('.list-group-item:nth-child(2)');
+item.style.backgroundColor = 'green';
+const itemInvi = document.querySelector('.list-group-item:nth-child(3)');
+itemInvi.style.display = 'none';
+const items = document.querySelectorAll('.list-group-item');
+items[1].style.color = 'green';
+
+const odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
 }
+
+
 
 
 
