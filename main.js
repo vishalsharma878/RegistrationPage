67a0c6500df4870b9cddd72567c1b332f4876c2e
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-10 15:42:24 +0530

    Second commit

---
+++ main.js
@@ -0,0 +1,18 @@
+console.dir(document);
+console.log(document.domain);
+
+console.log(document.links)
+//
+const headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerHTML ='<h3>Hello</>'; 
+//headerTitle.style.borderBottom = 'solid 3px black';
+
+const addItem = document.querySelector('.title');
+
+addItem.style.fontFamily = 'bold';
+addItem.style.color ='green';
+
+
+
